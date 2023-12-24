5b72b12a1d1006b550f1e8e40421236daa503cef
Ali Hemmati <ali508.ah@gmail.com>
2023-12-24 16:37:48 +0330

    Calculator1

--- calculator.js
+++ calculator.js
@@ -21,16 +21,16 @@ for (let button of buttons) {
   button.addEventListener("click", buttonPressed);
 }
 
-function operators (ali) {
-  let reza = '';
-  for(i = 0; i <= ali.length - 1; i++) {
+function operators (string) {
+  let newString = '';
+  for(i = 0; i <= string.length - 1; i++) {
     if(ali[i] === '*'){
-      reza += `${ali[i] = '&#215;'}`;
+      newString += `${string[i] = '&#215;'}`;
     }else if (ali[i] === '/'){
-      reza += `${ali[i] = '&#247;'}`;
+      newString += `${string[i] = '&#247;'}`;
     }else{
-      reza += ali[i];
+      newString += string[i];
     }
   }
-  return reza;
+  return newString;
 }
